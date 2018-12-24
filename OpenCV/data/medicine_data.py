36ee5c02efaf30ae7236dc9ec5f958f579c5db48
vaderwang <2953381201@qq.com>
2018-12-24 16:32:58 +0800

    Finish Process Data

--- OpenCV/data/medicine_data.py
+++ OpenCV/data/medicine_data.py
@@ -8,8 +8,6 @@ from sklearn.metrics import confusion_matrix
 class MedicineData:
     def __init__(self, data_dir='/home/vaderwang/Downloads/chest_xray'):
 
-        self.test_ratio = 0.2
-
         self.train_ratio = 0.8
 
         self.data_dir = data_dir
@@ -21,7 +19,10 @@ class MedicineData:
         self.normal_data = self.merge_folder_list(self.normal_folder_list)
 
         self.pneumonia_data = self.merge_folder_list(self.pneumonia_folder_list)
-        pass
+
+        self.train_normal, self.train_pneumonia, self.test_normal, self.test_pneumonia = self.build_data()
+
+        self.train_normal_labels, self.train_pneumonia_labels, self.test_normal_labels, self.test_pneumonia_labels = self.build_labels()
 
     def generate_folder_list(self, image_type='NORMAL'):
         first_level_folder_list = ['test', 'train', 'val']
@@ -42,115 +43,33 @@ class MedicineData:
         remove_str = '.DS_Store'
         return [data for data in data_list if not data.endswith(remove_str)]
 
-    # def build_data(self):
-    #     # 使用train set的部分数据作为train set
-    #     # 部分数据作为 none label  test 的数据作为none label
-    #     test_normal = os.listdir(self.test_normal_dir)
-    #     test_normal = [os.path.join(self.test_normal_dir, _) for _ in test_normal]
-    #
-    #     test_pneumonia = os.listdir(self.test_pneumonia_dir)
-    #     test_pneumonia = [os.path.join(self.test_pneumonia_dir, _) for _ in test_pneumonia]
-    #
-    #     # 使用训练集的部分数据作为没有标签的数据，部分数据作为test集
-    #     train_normal_full = os.listdir(self.train_normal_dir)
-    #     train_normal_full = [os.path.join(self.train_normal_dir, _) for _ in train_normal_full]
-    #
-    #     train_pneumonia_full = os.listdir(self.train_pneumonia_dir)
-    #     train_pneumonia_full = [os.path.join(self.train_pneumonia_dir, _) for _ in train_pneumonia_full]
-    #
-    #     # none_label_normal = train_normal_full[self.label_normal:]
-    #     # none_label_pneumonia = train_normal_full[self.label_pneumonia:]
-    #     # 得到全部的data, 分别shuffle 然后 split shuffle_indices = np.random.permutation(np.arange(data_size))
-    #     normal = test_normal + train_normal_full
-    #     random.shuffle(normal)
-    #     # normal = normal
-    #     # print('Normal:', len(normal))
-    #     """
-    #     Normal: 1575
-    #     Pneumonia: 4265
-    #     """
-    #     pneumonia = test_pneumonia + train_pneumonia_full
-    #     random.shuffle(pneumonia)
-    #     # start
-    #     test_normal_size = len(normal) * self.test_ratio
-    #     test_pneumonia_size = len(pneumonia) * self.test_ratio
-    #
-    #     # print('Pneumonia:', len(pneumonia))
-    #     none_label_data = train_normal_full + train_normal_full + test_normal + test_pneumonia
-    #     # none_label_data = train_normal + train_pneumonia + test_normal_full + test_pneumonia_full
-    #
-    #     # train_normal = train_normal_full[:self.label_normal]
-    #     # train_pneumonia = train_pneumonia_full[:self.label_pneumonia]
-    #
-    #     train_normal = train_normal_full
-    #     train_pneumonia = train_pneumonia_full
-    #
-    #     return train_normal, train_pneumonia, test_normal, test_pneumonia, none_label_data
-    #     pass
-    #
-    # def build_data_v2(self):
-    #     # 使用train set的部分数据作为train set
-    #     # 部分数据作为 none label  test 的数据作为none label
-    #     test_normal = os.listdir(self.test_normal_dir)
-    #     test_normal = [os.path.join(self.test_normal_dir, _) for _ in test_normal]
-    #
-    #     test_pneumonia = os.listdir(self.test_pneumonia_dir)
-    #     test_pneumonia = [os.path.join(self.test_pneumonia_dir, _) for _ in test_pneumonia]
-    #
-    #     # 使用训练集的部分数据作为没有标签的数据，部分数据作为test集
-    #     train_normal_full = os.listdir(self.train_normal_dir)
-    #     train_normal_full = [os.path.join(self.train_normal_dir, _) for _ in train_normal_full]
-    #
-    #     train_pneumonia_full = os.listdir(self.train_pneumonia_dir)
-    #     train_pneumonia_full = [os.path.join(self.train_pneumonia_dir, _) for _ in train_pneumonia_full]
-    #
-    #     # none_label_normal = train_normal_full[self.label_normal:]
-    #     # none_label_pneumonia = train_normal_full[self.label_pneumonia:]
-    #     # 得到全部的data, 分别shuffle 然后 split shuffle_indices = np.random.permutation(np.arange(data_size))
-    #     normal = test_normal + train_normal_full
-    #     random.shuffle(normal)
-    #
-    #     pneumonia = test_pneumonia + train_pneumonia_full
-    #     random.shuffle(pneumonia)
-    #     """
-    #            Normal: 1575
-    #            Pneumonia: 4265
-    #     """
-    #     # start
-    #     test_normal_size = int(len(normal) * self.test_ratio)
-    #     test_pneumonia_size = int(len(pneumonia) * self.test_ratio)
-    #
-    #     train_normal_size = int(len(normal) * self.train_ratio)
-    #     train_pneumonia_size = int(len(pneumonia) * self.train_ratio)
-    #
-    #     test_normal = normal[: test_normal_size]
-    #     test_pneumonia = pneumonia[: test_pneumonia_size]
-    #     print('Test Info : normal', len(test_normal), 'pneumonia', len(test_pneumonia))
-    #
-    #     train_normal = normal[test_normal_size: test_normal_size + train_normal_size]
-    #     train_pneumonia = pneumonia[test_pneumonia_size: test_pneumonia_size + train_pneumonia_size]
-    #     print('Train Info : normal', len(train_normal), 'pneumonia', len(train_pneumonia))
-    #
-    #     none_label_data = normal[test_normal_size + train_normal_size:] + pneumonia[test_pneumonia_size + train_normal_size:] + train_normal + train_pneumonia
-    #     print('None label Info: ', len(none_label_data))
-    #
-    #     return train_normal, train_pneumonia, test_normal, test_pneumonia, none_label_data
-    #
-    # def build_labels(self):
-    #     # 0 normal 1 pneumonia
-    #     # [1, 0] for normal, [0, 1] for pneumonia [0, 0] for none label data
-    #     train_normal_labels = [[1, 0] for _ in self.train_normal]
-    #
-    #     train_pneumonia_labels = [[0, 1] for _ in self.train_pneumonia]
-    #
-    #     test_normal_labels = [[1, 0] for _ in self.test_normal]
-    #
-    #     test_pneumonia_labels = [[0, 1] for _ in self.test_pneumonia]
-    #
-    #     none_label_labels = [[0, 0] for _ in self.none_label_data]
-    #
-    #     return train_normal_labels, train_pneumonia_labels, test_normal_labels, test_pneumonia_labels, none_label_labels
-    #     pass
+    def build_data(self):
+
+        random.shuffle(self.normal_data)
+        random.shuffle(self.pneumonia_data)
+
+        normal_size = int(len(self.normal_data) * self.train_ratio)
+        pneumonia_size = int(len(self.pneumonia_data) * self.train_ratio)
+
+        train_normal = self.normal_data[:normal_size]
+        train_pneumonia = self.pneumonia_data[:pneumonia_size]
+
+        test_normal = self.normal_data[normal_size:]
+        test_pneumonia = self.pneumonia_data[pneumonia_size:]
+
+        return train_normal, train_pneumonia, test_normal, test_pneumonia
+
+    def build_labels(self):
+        # [1, 0] for normal, [0, 1] for pneumonia
+        train_normal_labels = [[1, 0] for _ in self.train_normal]
+
+        train_pneumonia_labels = [[0, 1] for _ in self.train_pneumonia]
+
+        test_normal_labels = [[1, 0] for _ in self.test_normal]
+
+        test_pneumonia_labels = [[0, 1] for _ in self.test_pneumonia]
+
+        return train_normal_labels, train_pneumonia_labels, test_normal_labels, test_pneumonia_labels
 
     @staticmethod
     def convert_img(img_dirs):
@@ -164,76 +83,44 @@ class MedicineData:
         images = [parse_img(img_dir) for img_dir in img_dirs]
         return np.concatenate(images)
 
-    # def batch_iter(self, data_type, batch_size, shuffle=True):
-    #
-    #     data = np.array(self.none_label_data)
-    #     labels = np.array(self.none_label_labels)
-    #
-    #     if data_type == 'train':
-    #
-    #         data = np.array(self.train_normal + self.train_pneumonia)
-    #         labels = np.array(self.train_normal_labels + self.train_pneumonia_labels)
-    #
-    #     if data_type == 'test':
-    #
-    #         data = np.array(self.test_normal + self.test_pneumonia)
-    #         labels = np.array(self.test_normal_labels + self.test_pneumonia_labels)
-    #
-    #     # shuffle data
-    #     # print('Data: ', data.shape)
-    #     # print('Labels', labels.shape)
-    #     data_size = len(data)
-    #     num_batches = ((data_size-1)//batch_size) + 1
-    #     if shuffle:
-    #         shuffle_indices = np.random.permutation(np.arange(data_size))
-    #         shuffle_x = data[shuffle_indices]
-    #         shuffle_y = labels[shuffle_indices]
-    #     else:
-    #         shuffle_x = data
-    #         shuffle_y = labels
-    #     # 0 normal 1 pneumonia
-    #     # [1, 0] for normal, [0, 1] for pneumonia [0, 0] for none label data
-    #     # [[0] if label == [1] for label in shuffle_y]
-    #     # print('=================>')
-    #     # demo = [list(label) for label in shuffle_y]
-    #     self.true_labels = [[0] if list(label) == [1, 0] else [1] for label in shuffle_y]
-    #     # print(self.true_labels)
-    #     # print(demo)
-    #     # print('<=================')
-    #     # []
-    #     # print(shuffle_y)
-    #     for batch_num in range(num_batches):
-    #         start_index = batch_num*batch_size
-    #         end_index = min((batch_num+1) * batch_size, data_size)
-    #
-    #         batch_x = self.convert_img(shuffle_x[start_index:end_index])
-    #         batch_y = shuffle_y[start_index:end_index]
-    #
-    #         yield batch_x, batch_y
-    #     pass
-    #
-    # def cal_labels(self, predict_results):
-    #     """
-    #     R 语言里面，好像直接调一个函数confusionMatrix(predictions$Class_predicted, predictions$Class_actual, positive = 'Pneumonia') 就可以跑出一堆指标
-    #     指标 1、accuracy 2、precision； 3 recall （recall = sensitivity = true positive rate） ；   4、 specificity； 5  F-score
-    #     :return:
-    #     """
-    #     true_labels = np.array(self.true_labels)
-    #     predict_results = predict_results.reshape(-1, 1)
-    #     cnf_matrix = confusion_matrix(true_labels, predict_results)
-    #     print(cnf_matrix)
-    #     return cnf_matrix
-    #     pass
+    def batch_iter(self, batch_size, data_type='train', shuffle=True):
+
+        data = np.array(self.train_normal + self.train_pneumonia)
+        labels = np.array(self.train_normal_labels + self.train_pneumonia_labels)
+
+        if data_type == 'train':
+            pass
+
+        if data_type == 'test':
+            data = np.array(self.test_normal + self.test_pneumonia)
+            labels = np.array(self.test_normal_labels + self.test_pneumonia_labels)
+
+        data_size = len(data)
+        num_batches = ((data_size-1) // batch_size) + 1
+
+        if shuffle:
+            shuffle_indices = np.random.permutation(np.arange(data_size))
+            shuffle_x = data[shuffle_indices]
+            shuffle_y = labels[shuffle_indices]
+        else:
+            shuffle_x = data
+            shuffle_y = labels
+
+        for batch_num in range(num_batches):
+            start_index = batch_num * batch_size
+            end_index = min((batch_num + 1) * batch_size, data_size)
+
+            batch_x = self.convert_img(shuffle_x[start_index: end_index])
+            batch_y = shuffle_y[start_index: end_index]
+
+            yield batch_x, batch_y
+        pass
 
 
 def main():
     data = MedicineData()
-    print(len(data.normal_data))
-    print(len(data.pneumonia_data))
-    # for e in range(20):
-    #     for batch_x, batch_y in data.batch_iter(data_type='test', batch_size=1):
-    #         pass
-    # pass
+    for batch_x, batch_y in data.batch_iter(50):
+        print(batch_x.shape, batch_y.shape)
 
 
 if __name__ == '__main__':
